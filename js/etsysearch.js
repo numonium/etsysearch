d62f513dd6dd88c340d31857f6391babe371481b
Eric Newman <eric@numonium.com>
2015-04-20 01:04:22 -0400

    ~EN: initial results listing

--- js/etsysearch.js
+++ js/etsysearch.js
@@ -15,15 +15,29 @@
 		this.cfg.hashbang = '#!';
 		
 		this.eles = {
-			search : (args && args.search.ele ? args.search.ele : '')
+			search : (args && args.search.ele ? args.search.ele : ''),
+			results : (args && args.search.results ? args.search.results : ''), 
 		};
 		
 		this.forms = {
 			search : (args && args.search.form ? args.search.form : '')
 		};
 		
+		this.str = {
+			titles : {
+				_stub : ' - EtsySearch&trade;',
+				err : 'Error :(',
+				loading : 'Searching for ' + _.str.token('q') + '...',
+				results : _.str.token('num') + ' Results for "' + _.str.token('q') + '"'
+			}
+		};
+		
+		this.tem = {
+			result : null
+		};
+		
 		this.urls = {
-			search : 'https://openapi.etsy.com/v2/listings/active.js?keywords=' + _.str.token('terms') + '&limit=12&includes=Images:1&api_key=' + _.str.token('key')
+			search : 'https://openapi.etsy.com/v2/listings/active.js?keywords=' + _.str.token('terms') + '&limit=32&includes=Images:1&api_key=' + _.str.token('key')
 		};
 		
 		this.init();
@@ -48,20 +62,38 @@
 			search : {
 				q : function(args){
 					var val = (args && args.q ? args.q : __t.eles.search.value);
+					__t.q = val;
+					
 					var q = _.str.tokenReplace(__t.urls.search,_.str.token('terms'),val);
 					
 					if(val && __t.eles.search && (__t.eles.search.value == '')){
 						__t.eles.search.value = val;
 					}
 					
+					_.log('api[etsysearch][q][?]',val,q);
+					
+					__t.title(_.str.tokenReplace(__t.str.titles.loading,_.str.token('q'),__t.q));
+					__t.eles.results.setAttribute('data-_-state','loading');
+					
 					$.ajax({
 						url : q,
 						dataType : 'jsonp',
 						success : function(data){
-							_.log('api[etsysearch][search][*]',q,data);
+							_.log('api[etsysearch][q][*]',q,data);
+							
+							return __t.results({
+								data : data,
+								type : (!data.ok ? 'err' : (data.count === 0 ? 'empty' : '*'))
+							});
+							
 						},
 						error : function(data){
-							_.log('api[etsysearch][search][*err]',q,data);
+							_.log('api[etsysearch][q][*err]',q,data);
+							
+							return __t.results({
+								data : data,
+								type : 'err'
+							});
 						}
 					});
 				},
@@ -76,7 +108,7 @@
 						val : this.eles.search.value
 					});
 					
-					_.log('#!!!',url);
+					_.log('api[etsysearch][q][->]',url);
 					window.location.href = url;
 					
 					return false;
@@ -88,6 +120,15 @@
 			if(this.cfg.key){
 				this.urls.search = _.str.tokenReplace(this.urls.search,_.str.token('key'),this.cfg.key);
 			}
+			
+			this.eles.resultsList = this.eles.results.getElementsByClassName('_-listing--results')[0];
+			
+			// get template for new result
+			var result = document.getElementsByClassName('_-form--search--results--results')[0].getElementsByClassName('_-listing--item _-blank')[0];
+			this.tem.result = result.cloneNode(true);
+			result.parentNode.removeChild(result);
+
+			_.log('@@@@',this.tem.result);
 
 			if(this.forms.search){
 				$(this.forms.search).submit($.proxy(this.e.search.submit,this));
@@ -97,6 +138,73 @@
 			
 			this.e.hashchange();
 
+		},
+		results : function(args){
+			if(!args){
+				return false;
+			}
+			
+			if(args.type == 'err'){
+				this.title(this.titles.err);
+			}else if(args.type == 'empty'){
+				
+				var title = _.str.tokenReplace(this.str.titles.results,_.str.token('num'),'No');
+				title = _.str.tokenReplace(title,_.str.token('q'),this.q);
+				
+				this.title(title);
+			}else{
+				var title = _.str.tokenReplace(this.str.titles.results,_.str.token('num'),args.data.count);
+				title = _.str.tokenReplace(title,_.str.token('q'),this.q);
+				this.title(title);
+			}
+			
+			this.eles.results.setAttribute('data-_-state', args.type);
+			
+			if(!args.data.count){
+				return false;
+			}
+			
+			var result = {};
+			__t.eles.resultsList.innerHTML='';
+			
+			for(var i in args.data.results){
+				if(!args.data.results.hasOwnProperty(i)){
+					continue;
+				}
+				
+				result._ = this.tem.result.cloneNode(true);
+				result.a = result._.getElementsByClassName('_-listing--item--link')[0];
+				result.a.href = args.data.results[i].url;
+				
+				result.title = result._.getElementsByClassName('_-listing--item--title')[0];
+				result.title.appendChild(document.createTextNode(args.data.results[i].title));
+				
+				_.log('###',result);
+				
+				__t.eles.resultsList.appendChild(result._);
+				
+			}
+			
+/*
+	
+			<a class="_-listing--item--link" href="">
+				<div class="_-listing--item--title--wrapper">
+					<h2 class="_-listing--item--title"></h2>
+				</div>
+				<div class="_-listing--item--cat--wrapper">
+					<h3 class="_-listing--item--cat"></h3>
+				</div>
+				<div class="_-listing--item--content--wrapper">
+					<div class="_-listing--item--content"></div>
+				</div>
+			</a>
+*/			
+
+			
+			_.log('api[etsysearch][results]',this,args.data,args.type);
+		},
+		title : function(str){
+			document.title = str + this.str.titles._stub;	
 		},
 		url : function(args,remote){
 			if(!args || !args.svc){
@@ -125,7 +233,8 @@
 			},
 			search : {
 				ele : $('._-form._-form--search ._-form--search--q').get(0),
-				form : $('._-form._-form--search').get(0)
+				form : $('._-form._-form--search').get(0),
+				results : $('._-form--search--results').get(0)
 			}
 		});
 		
