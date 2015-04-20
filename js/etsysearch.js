01b9af6ba1f3ee82b1f1af08861233fa2ac05cb6
Eric Newman <eric@numonium.com>
2015-04-20 13:47:22 -0400

    ~EN: documentation/comments

--- js/etsysearch.js
+++ js/etsysearch.js
@@ -2,18 +2,22 @@
 	
 	var __t;
 	
+	// ~EN: declare all APIs under _._
+	
 	_._.EtsySearch = function(args){
 		
-		__t = this;
+		__t = this; //scope abstractor
 
+		// basic app configuration
 		this.cfg = (args && args.cfg ? args.cfg : {
 			appName : '',
 			key : '',
 			sharedSecret : ''
 		});
 		
-		this.cfg.hashbang = '#!';
+		this.cfg.hashbang = '#!'; // use hashbang so google will index ajax results
 		
+		// elements to reference
 		this.eles = {
 			form : (args && args.search.form ? args.search.form : ''),
 			search : (args && args.search.ele ? args.search.ele : ''),
@@ -25,7 +29,7 @@
 			search : (args && args.search.form ? args.search.form : '')
 		};
 		
-		this.str = {
+		this.str = { // strings to use
 			titles : {
 				_stub : ' - EtsySearch&trade;',
 				err : 'Error :(',
@@ -35,7 +39,7 @@
 			}
 		};
 		
-		this.tem = {
+		this.tem = { // templates
 			result : null
 		};
 		
@@ -43,12 +47,16 @@
 			search : 'https://openapi.etsy.com/v2/listings/active.js?keywords=' + _.str.token('terms') + '&limit=32&includes=Images:1&api_key=' + _.str.token('key')
 		};
 		
-		this.init();
+		this.init(); // do all real stuff in init function
 		
 	};
 	
+	// _._.EtsySearch.prototype - to mamke this syntax more "beautiful", we declare the prototype like so, rather than having to do _._.EstySearch.prototype.func ...
 	var __proto = {
-		e : {
+		e : { // events
+			
+			/* hashchange(event): general router for window.hashchange events
+				- URL should be in form of "#!/$cmd/$val/* */
 			hashchange : function(e){
 				
 				var hash = window.location.hash.split('/');
@@ -70,18 +78,21 @@
 				
 				_.log('api[etsysearch][->] ERR',cmd,hash);
 			},
+			
+			/* reset(array): reset search fields/results/data/title */
 			reset : function(args){
 				this.eles.resultsList.innerHTML = '';
 				this.eles.search.value = '';
 				this.title(__t.str.titles.welcome);
 				this.eles.results.setAttribute('data-_-state','');
-				alert('ddd');
 			},
-			search : {
+			search : { // search events
+				/* q(array): make search query -> request to Etsy -> send response to results processor */
 				q : function(args){
 					var val = (args && args.q ? args.q : __t.eles.search.value);
 					__t.q = val;
 					
+					// prepare query
 					var q = _.str.tokenReplace(__t.urls.search,_.str.token('terms'),val);
 					
 					if(val && __t.eles.search && (__t.eles.search.value == '')){
@@ -116,6 +127,8 @@
 						}
 					});
 				},
+				
+				/* reset(event): process "reset" button click -> just reroute to #!/reset */
 				reset : function(e){
 					
 					window.location.href = __t.url({
@@ -123,6 +136,8 @@
 					});
 
 				},
+				
+				/* submit(event): process form submit event *or* debounced keyup event */
 				submit : function(e){
 					
 					if(!this.eles.search){
@@ -148,9 +163,11 @@
 				}	
 			}
 		},
+		
+		/* init(array): set up events and other init stuff so we can get to work! :) */
 		init : function(args){
 			
-			if(this.cfg.key){
+			if(this.cfg.key){ // prepare urls, replace tokens with API keys and other cfg data
 				this.urls.search = _.str.tokenReplace(this.urls.search,_.str.token('key'),this.cfg.key);
 			}
 			
@@ -161,23 +178,31 @@
 			this.tem.result = result.cloneNode(true);
 			result.parentNode.removeChild(result);
 
+			// set up form/search event
 			if(this.forms.search){
 				$(this.forms.search).submit($.proxy(this.e.search.submit,this));
 			}
 			
+			// set up debounced keyup event from query field
 			if(this.eles.search){
 				this.eles.search.onkeyup = _.debounce(function(e){
 					return __t.e.search.submit.call(__t,e);
 				},200);
 			}
 			
+			//hash change -> router
 			window.addEventListener('hashchange',this.e.hashchange);
 			
+			// reset button
 			this.eles.form.getElementsByClassName('_-input--reset')[0].onclick = this.e.search.reset;
 			
+			// use existing URL (if any) for initial query
 			this.e.hashchange();
 
 		},
+		
+		/* makeResult(obj) - take given "obj" and construct result element
+			@returns object with all result data -> result._ will be actual HTML element */
 		makeResult : function(data){
 			if(!data || (data.state == 'banned')){
 				return false;
@@ -185,6 +210,7 @@
 				
 			var result = {};
 			
+			// clone template
 			result._ = this.tem.result.cloneNode(true);
 			result._.setAttribute('data-_-item-uuid', data.listing_id);
 			result._.className = result._.className.replace(' _-blank','');
@@ -194,28 +220,31 @@
 			
 			(result.title = result._.getElementsByClassName('_-listing--item--title')[0]).innerHTML = data.title; // ~EN: use innerHTML here so we can transfer over HTML entities - would be escaped in TextNode
 			
-			
 			// ~EN: create text outline "shadow"
 			result.title2 = result.title.cloneNode(true);
 			result.title2.className += ' _-text--outline';
 			result.title.parentNode.appendChild(result.title2);
 			
+			// category datqa
 			result.cat = result._.getElementsByClassName('_-listing--item--cat')[0];
 			var tmp = document.createElement('strong');
 			tmp.appendChild(document.createTextNode(data.category_path ? data.category_path[0] : 'Unknown'));
 			result.cat.appendChild(tmp);
 			
+			// price
 			result.cat.appendChild(document.createTextNode(' \u2013 '));
 			tmp = document.createElement('span');
 			tmp.className = '_-listing--item--price';
 			tmp.appendChild(document.createTextNode('$' + data.price));
 			result.cat.appendChild(tmp);
 			
+			// content
 			result.content = result._.getElementsByClassName('_-listing--item--content')[0];
 			tmp = document.createElement('p');
 			tmp.innerHTML = data.description;
 			result.content.appendChild(tmp);
 			
+			// img
 			result.img = result._.getElementsByClassName('_-listing--item--img')[0].getElementsByTagName('img')[0];
 			result.img.src = data.Images[0].url_170x135;
 						
@@ -224,11 +253,14 @@
 			return result;
 
 		},
+		
+		/* results(array): prepare and print out all results from prior request */
 		results : function(args){
 			if(!args){
 				return false;
 			}
 			
+			// change page title
 			if(args.type == 'err'){
 				this.title(this.titles.err);
 			}else if(args.type == 'empty'){
@@ -243,6 +275,7 @@
 				this.title(title);
 			}
 			
+			// change results state
 			this.eles.results.setAttribute('data-_-state', args.type);			
 			this.eles.title.innerHTML = (args.data.count === 0 ? 'No' : _.str.numberFormat(args.data.count)) + ' Result' + (args.data.count !== 1 ? 's': '') + ' Found for &ldquo;' + this.q + '&rdquo;' + (!args.data.count ? ' :(' : ':');
 			
@@ -253,6 +286,7 @@
 			var result = null;
 			this.eles.resultsList.innerHTML = '';
 			
+			// loop through results and append new result to list
 			for(var i in args.data.results){
 				if(!args.data.results.hasOwnProperty(i)){
 					continue;
@@ -266,9 +300,16 @@
 			
 			_.log('api[etsysearch][results]',this,args.data,args.type);
 		},
+		
+		/* title(str): generate string for page title */
 		title : function(str){
 			document.title = str + this.str.titles._stub;	
 		},
+		
+		/* url(array, bool): url abstractor so we don't hardcode URLs to use for internal/external routing
+			-> most URLs are generated off of the "args.svc" property
+			-> can throw any data into "args"
+			-> "remote" toggles between local (hash change) or remote requests (location change) */
 		url : function(args,remote){
 			if(!args || !args.svc){
 				return false;
@@ -280,12 +321,14 @@
 		}
 	};
 	
+	// ~EN: add prototype to class */
 	for(var i in __proto){
 		if(__proto.hasOwnProperty(i)){
 			_._.EtsySearch.prototype[i] = __proto[i];
 		}
 	}
 	
+	// ~EN: init everything -> we only really use jQuery for document.ready and some function proxying
 	$(document).ready(function(){
 	
 		_.api.etsysearch = new _._.EtsySearch({
@@ -297,8 +340,8 @@
 			search : {
 				ele : document.getElementsByClassName('_-form _-form--search')[0].getElementsByClassName('_-form--search--q')[0],
 				title : document.getElementsByClassName('_-page--title')[0],
-				form : $('._-form._-form--search').get(0),
-				results : $('._-form--search--results').get(0)
+				form : document.getElementsByClassName('_-form _-form--search')[0],
+				results : document.getElementsByClassName('_-form--search--results')[0]
 			}
 		});
 		
